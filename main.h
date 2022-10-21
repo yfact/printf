82161af035ad38243cde1f940e4d51d0a5364945
Olabamiji Yusuf <yfact247@gmail.com>
2022-10-21 22:22:48 +0000

    print few functions

--- main.h
+++ main.h
@@ -3,7 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * struct printer - conects format specifier with its corresponding function.
+ * @flag: format specifier.
+ * @function: pointer to @flag especific function.
+ */
+typedef struct printer
+{
+	char flag;
+	int (*function)(va_list);
+} printer_t;
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_c(va_list arg);
+int print_s(va_list arg);
+int print_i(va_list arg);
 #endif
