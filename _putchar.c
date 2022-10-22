ffb9ae7a9d4bc0bb4fdb20aef95869b801095fdb
Olabamiji Yusuf <yfact247@gmail.com>
2022-10-22 04:44:59 +0000

    update on putchar and other functions

--- _putchar.c
+++ _putchar.c
@@ -9,3 +9,85 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+/**
+ * printc - Entry function. Print character
+ * @list: variable va_list
+ *
+ * Return: 1 (nbyte)
+ */
+int printc(va_list list)
+{
+	_putchar(va_arg(list, int));
+	return (1);
+}
+/**
+ * print_string - Entry point. Print string
+ * @s: variable va_list
+ *
+ * Return: k (nbytes) 6 (NULL)
+ */
+int print_string(va_list s)
+{
+	char *str;
+	int k;
+
+	str = va_arg(s, char*);
+	if (str == NULL)
+	{
+	write(1, "(null)", 6);
+	return (6);
+	}
+	else
+	{
+	for (k = 0; str[k] != '\0'; k++)
+	{
+	_putchar(str[k]);
+	}
+	}
+	return (k);
+}
+/**
+ * print_n - Entry point. Print number
+ * @n: Variable va_list
+ *
+ * Return: count (nbytes)
+ */
+int print_n(va_list n)
+{
+	long int number;
+	int counter, aux_variable, base;
+
+	counter = 0;
+	number = va_arg(n, int);
+
+	if (number < 0)
+	{
+	number *= -1;
+	_putchar(45);
+	counter++;
+	}
+	if (number >= 0 && number <= 9)
+	{
+	_putchar(number + 48);
+	counter++;
+	}
+	if (number > 9)
+	{
+	base = 10;
+
+	while (number / base > 9)
+	{
+	base *= 10;
+	}
+
+	while (base > 0)
+	{
+	aux_variable = number / base;
+	number = number % base;
+	_putchar(aux_variable + 48);
+	base = base / 10;
+	counter++;
+	}
+	}
+	return (counter);
+}
